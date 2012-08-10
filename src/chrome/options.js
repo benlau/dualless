d96ca3666dd0a1a1b1d2bf61a5cc65fe864d9a2b
Ben Lau <xbenlau@gmail.com>
2012-08-10 23:39:35 +0800

    In options page , added a function to reset viewport

--- src/chrome/options.js
+++ src/chrome/options.js
@@ -2,6 +2,9 @@
 function OptionsController($scope) {
 	/* Pairing Mode */
 	
+	var bg = chrome.extension.getBackgroundPage();
+	var manager = bg.manager();
+	
 	function updatePairingButton(val) {
 		var list;
 		if (val > 0) { // By default , the pairing mode is on.
@@ -11,11 +14,21 @@ function OptionsController($scope) {
 		}
 		$scope.pairingButtonState = list;
 	}
+	
+	function updateViewport(){
+		$scope.viewport = manager.viewport().size().toString();	
+	}
 
 	$scope.setPairingMode = function(val){
 		localStorage.pairingModeEnabled = val;
 		updatePairingButton(val);
 	};
 	
+	$scope.resetViewport = function(){
+		manager.viewport().reset();
+		updateViewport();
+	};
+		
+	updateViewport();
 	updatePairingButton(localStorage.pairingModeEnabled);
 }

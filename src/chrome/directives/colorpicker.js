932041b058b177666e1be53ae91ba4f756b68a19
Ben Lau <xbenlau@gmail.com>
2013-06-11 23:59:45 +0800

    colorpicker is completed

--- src/chrome/directives/colorpicker.js
+++ src/chrome/directives/colorpicker.js
@@ -5,15 +5,44 @@
 define(["module",
         "dualless/lib/bootstrap-colorpicker"],
          function(self) {
+             
+    function Controller($scope,$element){
+        var picker = $($element).colorpicker();
+        
+        $scope.$watch("value",function(){
+            /*
+            $scope.$evalAsync(function() {
+                $($element).colorpicker('setValue',$scope.value);
+            });
+            */
+//            $element.val($scope.value);
+//            $element.data('colorpicker').color.setColor($scope.value);
+            $($element).colorpicker('setValue',$scope.value);
+        });
+
+        picker.on('changeColor',function(ev) {
+
+            var v = ev.color.toHex();
+            if (v == $scope.value)
+                return;
+            $scope.$apply(function() {
+                $scope.value = v;
+            });
+
+        });
+    }
     
     function factory() {
         var def = {
-            replace: false,
+            replace: true,
             transclude: false,
             restrict : 'A',
-            require : "?ngModel",
+            template: "<div data-color-format='hex' data-color='' style='background-color:{{value}}'></div>",
+            scope : {
+                value : "=value"
+            },
+            controller : Controller,
             link : function(scope, element, attrs, ngModel) {
-                $(element).colorpicker();
             }
         };
         return def;

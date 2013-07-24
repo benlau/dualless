baac1591fcca3e5c19f07520f069f9cc041a5bd3
Ben Lau <xbenlau@gmail.com>
2013-07-25 01:39:11 +0800

    views/bookmark : A "open" button is added to launch the selected link on bookmark editor directly

--- src/chrome/views/bookmark.js
+++ src/chrome/views/bookmark.js
@@ -108,6 +108,24 @@ define(["module",
              $scope.$evalAsync();
          };
          
+         /** Open the selected link on new tab and split
+          */
+         $scope.open = function() {
+             console.log("open",$scope.selectedLink);
+             var options = {};
+             options.param1 =  parseInt($scope.param1);
+             options.param2 =  parseInt($scope.param2);
+             options.orientation = $scope.orientation;
+             options.position = $scope.position;
+             options.action = {};
+             options.action.link = $scope.selectedLink;
+                  
+             if ($scope.selectedLink !== undefined) {
+                 console.log("split",options);
+                 WindowManager.split(options);                
+             }
+         }
+         
     }
     
     Controller.inject = ["$scope" , 
